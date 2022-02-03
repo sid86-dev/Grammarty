6ed2c89f445f27b193539b752ec15a3ec39880e6
sid86-dev <sid86harth@gmail.com>
2022-02-04 00:49:30 +0530

    migrate to languagetool

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-from gingerit.gingerit import GingerIt
-
+import language_tool_python
 from flask import Flask, request,render_template,redirect, make_response, jsonify
-import json
 
 app = Flask(__name__)
  
 
 def checkText(text):
-    parser = GingerIt()
-    newText = parser.parse(text)
+    tool = language_tool_python.LanguageTool('en-US', config={ 'cacheSize': 1000, 'pipelineCaching': True })
+    newText = tool.correct(text)
     return newText
 
-@app.route("/")
+@app.get("/")
 def sent():
     if request.method == "GET":
         return render_template("index.html")
@@ -28,10 +26,8 @@ def sent_correct():
     # get data from frontend
     data = request.get_json()
     text = data["text"]
-    print(text)
 
     try:
-        print(checkText(text))
         res = make_response(jsonify({"error": 'no-error','text':checkText(text)}), 200)
         return res
     except:
@@ -41,5 +37,4 @@ def sent_correct():
         
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    print(checkText("hi this are tree"))
+    app.run(debug=True)
